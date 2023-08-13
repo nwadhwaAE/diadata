d1f11080f8fb64a0b3fd53d29e0e48d9d4133d64
nwadhwaAE <nitswadhwa@gmail.com>
2023-08-14 02:10:26 +0530

    rep update

--- utils/discord_integration.py
+++ utils/discord_integration.py
@@ -17,14 +17,14 @@ def send_discord_message(message):
 
 def discord_message_comparison(fantom_dextools_msg, fantom_coingecko_msg, polygon_dextools_msg, polygon_coingecko_msg):
     message = (
-        f"*************************************************\n"
+        f"**\n"
         f"Fantom MiMATIC price comparison:\n"
         f"DIA vs DEXTools: {fantom_dextools_msg}\n"
         f"DIA vs CoinGecko: {fantom_coingecko_msg}\n\n"
         f"Polygon MiMATIC price comparison:\n"
         f"DIA vs DEXTools: {polygon_dextools_msg}\n"
-        f"DIA vs CoinGecko: {polygon_coingecko_msg}"
-        f"*************************************************\n"
+        f"DIA vs CoinGecko: {polygon_coingecko_msg}\n"
+        f"**\n"
     )
 
     send_discord_message(message)
@@ -33,7 +33,7 @@ def discord_message_price(dia_fantom_price, dia_polygon_price,
         dextools_fantom_price, dextools_polygon_price,
         coingecko_fantom_price, coingecko_polygon_price):
     message = (
-        f"*************************************************\n"
+        f"**\n"
         f"MiMATIC DIA price:\n"
         f"Fantom: {dia_fantom_price:.2f}\n"
         f"Polygon: {dia_polygon_price:.2f}\n\n"
@@ -42,8 +42,8 @@ def discord_message_price(dia_fantom_price, dia_polygon_price,
         f"Polygon: {dextools_polygon_price:.2f}\n\n"
         f"MiMATIC Coingecko price:\n"
         f"Fantom: {coingecko_fantom_price:.2f}\n"
-        f"Polygon: {coingecko_polygon_price:.2f}\n\n"
-        f"*************************************************\n"
+        f"Polygon: {coingecko_polygon_price:.2f}\n"
+        f"**\n"
 
     )
 
